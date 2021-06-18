b7b9c55cd29f85e13555926fbd95ffb4e32568b0
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2021-06-18 11:14:00 -0300

    Just stop reading these for a while, ok..

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{env::current_dir, io};
+
 fn main() {
     println!("Hello, world!");
+    
+    println!("Guess the number!");
+    
+    println!("Please input your guess.");
+
+    let dir = current_dir().expect("Cant read the current path");
+    let mut guess = String::new();
+
+    println!("Current dir is: {:?}", dir);
+    
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Filed to read line");
+    
+    println!("You guessed: {}", guess)
 }
